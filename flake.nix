00b61306b04ff7ab764729b1edc81579ef369ebc
J. Dekker <jdek@itanimul.li>
2026-04-06 12:07:34 +0200

    feat(flake): init

---
+++ flake.nix
@@ -0,0 +1,6 @@
+{
+  inputs.flakelight-rust.url = "github:accelbread/flakelight-rust";
+  outputs = { flakelight-rust, ... }: flakelight-rust ./. {
+    systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux"];
+  };
+}
